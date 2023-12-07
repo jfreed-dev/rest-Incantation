0bf5b626138719eeb9a3152d32a84712b6ad81a1
Jonathan Freed <53282525+jfreed-dev@users.noreply.github.com>
2023-12-07 16:30:11 -0500

    Updated code and fix errors.  Will provide better GIT commits messages when there is an initial working version of all the code.

--- credential_method.py
+++ credential_method.py
@@ -1,4 +1,7 @@
-def find_authentication_method(api_documentation):
+import logging
+from typing import Any, Dict, List, Tuple, Union
+
+def find_authentication_method(api_documentation: Dict[str, Any]) -> Union[str, List[Tuple[str, Any]]]:
     """
     Find the authentication method used by the API from its OpenAPI documentation.
     """
@@ -6,16 +9,14 @@ def find_authentication_method(api_documentation):
         security_schemes = api_documentation.get('components', {}).get('securitySchemes', {})
         if not security_schemes:
             return "No authentication method found in documentation."
-
         auth_methods = []
         for name, details in security_schemes.items():
             auth_type = details.get('type')
             auth_methods.append((name, auth_type))
-
         return auth_methods
     except Exception as e:
-        print(f"Error processing API documentation: {e}")
-        return []
+        logging.error(f"Error processing API documentation: {e}")
+        return "Error processing API documentation."
 
 # Example usage
 # Assuming api_documentation is a dictionary containing the parsed OpenAPI documentation
--- load_openapi_documentation.py
+++ load_openapi_documentation.py
@@ -1,15 +1,14 @@
+from typing import Union
 import json
-from typing import Any
 import yaml
-import argparse
+import logging
 
-
-def load_openapi_documentation(file_path: object) -> object:
+def load_openapi_documentation(file_path: str) -> Union[dict, list, None]:
     """
+    Load OpenAPI documentation from a JSON or YAML file.
 
-    :type file_path: object
-    :param file_path:
-    :return:
+    :param file_path: The path to the file.
+    :return: The loaded data as a dictionary or list, or None in case of an error.
     """
     try:
         if file_path.endswith('.yaml') or file_path.endswith('.yml'):
@@ -20,27 +19,6 @@ def load_openapi_documentation(file_path: object) -> object:
                 return json.load(file)
         else:
             raise ValueError("Unsupported file format")
-    except Exception as e:
-        print(f"Error loading OpenAPI documentation: {e}")
+    except (OSError, IOError, yaml.YAMLError, json.JSONDecodeError) as e:
+        logging.error(f"Error loading OpenAPI documentation: {e}")
         return None
-
-
-def get_file_path_interactive():
-    return input("Enter the path to the OpenAPI documentation file: ")
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Load OpenAPI documentation.')
-    parser.add_argument('file_path', nargs='?', help='Path to the OpenAPI documentation file (JSON or YAML)')
-    return parser.parse_args()
-
-
-# Main Execution
-if __name__ == "__main__":
-    args = parse_arguments()
-    # If the script is run without any arguments, it will prompt for file path
-    file_path = args.file_path if args.file_path else get_file_path_interactive()
-    api_documentation: Any | None = load_openapi_documentation(file_path)
-    if api_documentation:
-        print("OpenAPI Documentation loaded successfully!")
-        # Further processing of the api_documentation dictionary can be done here
